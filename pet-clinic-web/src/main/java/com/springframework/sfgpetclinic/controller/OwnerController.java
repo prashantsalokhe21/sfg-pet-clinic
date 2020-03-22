657d2c3a2881bb76b358e6d3f1c7e3e88c407d37
Prashant Salokhe <p.salokhe21@gmail.com>
2020-03-22 19:38:11 +0530

    Added owner index and controller. Closes #15

---
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controller/OwnerController.java
@@ -0,0 +1,14 @@
+package com.springframework.sfgpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+	
+	@RequestMapping({"/owners","/owners/index","owners/index.html"})
+	public String ownerList() {
+		return "/owners/index";
+	}
+
+}
