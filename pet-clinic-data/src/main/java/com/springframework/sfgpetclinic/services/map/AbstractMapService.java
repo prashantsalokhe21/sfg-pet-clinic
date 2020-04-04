a69a1a02d959431056274d807b99a83e8d701eaa
Prashant Salokhe <p.salokhe21@gmail.com>
2020-04-04 20:31:15 +0530

    Removed commented code.

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -63,23 +63,4 @@ public class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
 		return nextId;
 	}
-
-	/*
-	 * public T save(T object) { if (object != null) { if (object.getId() == null) {
-	 * object.setId(getNextId()); } map.put(object.getId(), object);
-	 * 
-	 * } else { throw new RuntimeException("Object can not be null!"); } return
-	 * object; }
-	 */
-
-	/*
-	 * private Long getNextId() {
-	 * 
-	 * Long nextId = null;
-	 * 
-	 * try { nextId = Collections.max(map.keySet()) + 1; } catch
-	 * (NoSuchElementException e) { return 1L; }
-	 * 
-	 * return nextId; }
-	 */
 }
