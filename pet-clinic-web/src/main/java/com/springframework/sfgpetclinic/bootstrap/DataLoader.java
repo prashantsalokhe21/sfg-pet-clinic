072266b90a2e1b7a269e6e63f7a587956f43ac08
Prashant Salokhe <p.salokhe21@gmail.com>
2020-04-18 21:11:33 +0530

    Enhanced Owners with contact info and Pets. Closes #38

--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.springframework.sfgpetclinic.bootstrap;
 
 import com.springframework.sfgpetclinic.model.Owner;
+import com.springframework.sfgpetclinic.model.Pet;
 import com.springframework.sfgpetclinic.model.PetType;
 import com.springframework.sfgpetclinic.model.Vet;
 import com.springframework.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.springframework.sfgpetclinic.services.PetTypeService;
 import com.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner{
@@ -37,13 +40,29 @@ public class DataLoader implements CommandLineRunner{
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Prashant");
 		owner1.setLastName("Salokhe");
-		
+		owner1.setAddress("10/2 Vishwadharam Warje");
+		owner1.setTelephone("8899562314");
+		owner1.setCity("Pune");
+		Pet prashantsPet = new Pet();
+		prashantsPet.setPetType(savedDogPetType);
+		prashantsPet.setOwner(owner1);
+		prashantsPet.setBirthDate(LocalDate.now());
+		prashantsPet.setName("Tommy");
+		owner1.getPets().add(prashantsPet);
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Kunal");
 		owner2.setLastName("Patil");
-		
+		owner2.setAddress("Flat No-6 Shantai Sinhgad Road");
+		owner2.setTelephone("9988441122");
+		owner2.setCity("Pune");
+		Pet kunalsPet = new Pet();
+		kunalsPet.setPetType(savedCatPetType);
+		kunalsPet.setOwner(owner2);
+		kunalsPet.setBirthDate(LocalDate.now());
+		kunalsPet.setName("furry");
+		owner2.getPets().add(kunalsPet);
 		ownerService.save(owner2);
 		
 		System.out.println("Loaded Owner data...");
