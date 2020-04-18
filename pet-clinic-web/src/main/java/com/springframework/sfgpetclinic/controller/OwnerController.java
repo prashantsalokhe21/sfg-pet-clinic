02d4b4643269ec1df32603cd1a67fc23f3e66756
Prashant Salokhe <p.salokhe21@gmail.com>
2020-04-19 00:44:32 +0530

    Fix broken menu links. Closes #50

--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controller/OwnerController.java
@@ -1,26 +1,31 @@
 package com.springframework.sfgpetclinic.controller;
 
+import com.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.springframework.sfgpetclinic.services.OwnerService;
-
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
-	
-	private final OwnerService ownerService;
-	
-	public OwnerController(OwnerService ownerService) {
-		super();
-		this.ownerService = ownerService;
-	}
 
-	@RequestMapping({"/owners","/owners/index","owners/index.html"})
-	public String ownerList(Model model) {
-		
-		model.addAttribute("owners", ownerService.findAll());
-		return "/owners/index";
-	}
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        super();
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String ownerList(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
+        return "/owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String findOwners() {
+        return "notimplemented";
+    }
+
 
 }
--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controller/VetController.java
@@ -1,26 +1,25 @@
 package com.springframework.sfgpetclinic.controller;
 
+import com.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.springframework.sfgpetclinic.services.VetService;
-
 @Controller
 public class VetController {
-	
-	private VetService vetService;
-	
-	public VetController(VetService vetService) {
-		super();
-		this.vetService = vetService;
-	}
 
-	@RequestMapping({ "/vets", "/vets/index", "/vets/index.html" })
-	public String listVets(Model model) {
-		
-		model.addAttribute("vets", vetService.findAll());
-		return "/vets/index";
-	}
+    private VetService vetService;
+
+    public VetController(VetService vetService) {
+        super();
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+        return "/vets/index";
+    }
 
 }
